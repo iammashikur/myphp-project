c1ed057a085b5660dad2b787a36164cddda00505
700mirash <700mirash@gmail.com>
2020-12-27 22:50:11 +0600

    Multi Dimensional Array

--- index.php
+++ index.php
@@ -1,9 +1,111 @@
 <?php
 
-echo 'mirash';
+$multyArray = array(
 
-function upload_photo(){
+    array(
+
+        'name' => 'Mirash',
+        'fathername' => 'demo',
+        'mothername' => 'dfsdfsd',
+    ),
+
+    array(
+
+        'name' => 'Ovi',
+        'fathername' => 'sdfsd',
+        'mothername' => 'fsdfsd',
+    ),
+
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+
+
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+    array(
+
+        'name' => 'Taz',
+        'fathername' => 'dfsdf',
+        'mothername' => 'dsfsdf',
+    ),
+
     
-}
+
+);
+
+
 
 ?>
+
+
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>Father</th>
+        <th>Mother</th>
+    </tr>
+
+    <?php foreach($multyArray as $data): ?>
+
+    <tr>
+        <td><?= $data['name'] ?></td>
+        <td><?= $data['fathername'] ?></td>
+        <td><?= $data['mothername'] ?></td>
+    </tr>
+
+    <?php endforeach; ?>
+
+</table>
